2ffaa8b812c59a1a356389806e5fd1c269ff23fd
Henrique Gottardi Werlang <henriquewerlang@hotmail.com>
2023-11-29 13:50:39 -0300

    Fix map file generation.

--- Pas2JS.Compiler.Delphi.pas
+++ Pas2JS.Compiler.Delphi.pas
@@ -104,7 +104,7 @@ begin
   var DestinyFile := TMemoryStream.Create;
   Result := True;
 
-  Writer.SaveJSToStream(False, MapFilename, DestinyFile);
+  Writer.SaveJSToStream(False, ExtractFileName(MapFilename), DestinyFile);
 
   DestinyFile.WriteData(#0);
 
